a31e46766b8cadb498aa87b6b10f55cf7dbb69f4
ArthurMeloS <81020132+ArthurMeloS@users.noreply.github.com>
2021-04-14 10:54:22 -0300

    Add files via upload

---
+++ atividade/questao2.py
@@ -0,0 +1,8 @@
+fila1 = [1, 2 ,3, 4, 5]
+print("Pilha", fila1)
+
+fila1.append(10)
+print("Adicionando elemento ao topo da pilha", fila1)
+
+fila1.pop(0)
+print("Removendo elemento do topo da pilha", fila1)
---
+++ atividade/questao3.py
@@ -0,0 +1,8 @@
+lista1 = [1, 2 ,3, 4, 5]
+print("Listaa", lista1)
+
+lista1.insert(2, 10)
+print("Adicionando elemento na lista encadeada", lista1)
+
+lista1.pop(1)
+print("Removendo elemento da lista encadeada", lista1)
